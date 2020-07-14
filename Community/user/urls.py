38e25d5feaef32706f4bb2983fe0fb292eb43dd5
Jun Young Jang <junyoungjang@Junui-MacBookPro.local>
2020-07-14 18:30:22 +0900

    more changes

--- Community/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .import views
-
-urlpatterns= [
-    path('register/',views.register),
-    path('login/',views.login),
-    path('logout/',views.logout),
-]
